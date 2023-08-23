cf00f989c5ccbc3f9331ff7ad504b39c67ac0b99
MackenzyCarrillo <709666@student.dusd.net>
2023-08-23 13:54:27 -0700

    [[
    [[

--- QuickTaskCarrilloP7/Assets/Scripts/Movement.cs
+++ QuickTaskCarrilloP7/Assets/Scripts/Movement.cs
@@ -4,30 +4,61 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float verticalInput;
-    public float horizontalInput;
-    public Rigidbody2D playerRb;
+
+    //Most of my code I found online because I mostly forgot how to add movement in 2D Unity
+
+    public float speed = 1.0f;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+    private float jumpPower = 10.0f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody2D>();
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-        if (Input.GetKeyDown(KeyCode.D))
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
         }
 
-        else if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-           
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        Flip();
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
 
         }
+
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 }
